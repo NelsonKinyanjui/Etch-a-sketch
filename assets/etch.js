f16085186cc74e999252c8b00e8bf69705a5d690
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-05-17 11:10:01 +0300

    add boxes

--- assets/etch.js
+++ assets/etch.js
@@ -1,4 +1,24 @@
- const container = document.createElement('div');
+ const container = document.querySelector('.container')
+  
+
+const numberOfDivs = 256;
+
+
+ for(i=0; i < numberOfDivs; i++){
+
+   const newDiv = document.createElement('div');
+   newDiv.style.border = '1px solid blue';
+   newDiv.style.margin= '4px';
+   newDiv.style.width = '40px'
+   newDiv.style.height = ' 40px'
+
+   
+ container.appendChild(newDiv);
+
+
+ }
  
+
+
 
  
